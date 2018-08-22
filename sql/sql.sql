aed2c8bd78414f4b58efdc6f8ae8030b11ce5290
= <reudismam@gmail.com>
2018-08-22 14:55:59 -0300

    Adding missed file.

--- sql/sql.sql
+++ sql/sql.sql
@@ -40,3 +40,22 @@ FROM     (
                   AND      e.context IS NOT NULL 
                   GROUP BY c.label) t 
 WHERE    s >= 2 
+
+#Select cluster, number of edits, number of projects, and number of developers.
+SELECT   * 
+FROM     ( 
+                  SELECT   c.label, 
+                           Count(*) AS s,
+                           Count(distinct(e.project)) as pj,
+                           Count(distinct(e.developer)) as dev
+                  FROM     cluster c, 
+                           cluster_edit ce, 
+                           edit e 
+                  WHERE    c.id = ce.cluster_id 
+                  AND      ce.nodes_id = e.id 
+                  AND      c.dst_id IS NOT NULL 
+                  AND      e.dst_id IS NOT NULL 
+                  AND      e.context IS NOT NULL 
+                  GROUP BY c.label) t 
+WHERE    s >= 2 
+ORDER BY pj DESC
